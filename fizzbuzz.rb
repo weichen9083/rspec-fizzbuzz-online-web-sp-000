3fc0323e47de31f098e9a0ca76e6d4c47ef8acc4
wei chen <weichen9083@gmail.com>
2019-08-20 22:28:47 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0 
-    puts "FizzBuzz"
+    return "FizzBuzz"
   elsif int % 3 == 0
-    puts "Fizz"
+    return "Fizz"
   elsif nt % 5 == 0 
-    puts "Buzz"
+    return "Buzz"
   else 
     return nil
 end 
