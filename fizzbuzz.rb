64b8797926b7d570cf32230a8714fca2d166cbda
wei chen <weichen9083@gmail.com>
2019-08-20 22:27:10 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 3 == 0 
-    puts "Fizz"
-  elsif int % 5 == 0 
-    puts "Buzz"
-  elsif int % 3 == 0 && int % 5 == 0 
+  if int % 3 == 0 && int % 5 == 0 
     puts "FizzBuzz"
+  elsif int % 3 == 0
+    puts "Fizz"
+  elsif nt % 5 == 0 
+    puts "Buzz"
   else 
+    return nil
     end 
